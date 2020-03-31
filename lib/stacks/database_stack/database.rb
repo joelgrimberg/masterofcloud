8c713eb6ce88d14519432308309147070d54dae8
Joel Grimberg <jgrimberg@xebia.com>
2020-03-31 11:51:43 +0200

    fixed Implementations Comment #2

--- lib/stacks/database_stack/database.rb
+++ lib/stacks/database_stack/database.rb
@@ -2,37 +2,31 @@ module DatabaseStack
     module Database
       extend ActiveSupport::Concern
       included do
-        parameter :dbname,
-                  default: "myAwesomeDatabase",
-                  description: "my fucking awesome database"
         parameter :engine_type,
                   default: "MySQL"
-        parameter :dballocatedstorage,
+        parameter :db_allocated_storage,
                   default: "50"
         parameter :subnets,
                   description: "Subnets for the Database"
 
-        resource :dbsubnetgroup,
+        resource :db_subnet_group,
                   type: "AWS::RDS::DBSubnetGroup" do |r|
-          r.property(:DBSubnetGroupDescription) {"subnetgroup for DB"}
-          r.property(:DBSubnetGroupName) {"dbsubnetgroup"}
-          r.property(:SubnetIds) {:subnets.ref.fnsplit(",")}
-                  end
+          r.property(:db_subnet_groupname) {"db_subnet_group"}
+          r.property(:db_subnet_groupdescription) {"subnetgroup for DB"}
+          r.property(:subnet_ids) {:subnets.ref.fnsplit(",")}
+        end
 
-        resource :mydatabase,
+        resource :my_database,
                   type: "AWS::RDS::DBInstance" do |r|
-          r.property(:DBInstanceClass) {"db.t2.micro"}
-          r.property(:DBSubnetGroupName) {:dbsubnetgroup.ref}
-          r.property(:Engine) {:engine_type.ref}
-          r.property(:MasterUsername) { ENV["DB_Username"] }
-          r.property(:MasterUserPassword) { ENV["DB_Password"] }
-          r.property(:AllocatedStorage) {:dballocatedstorage.ref}
-          r.property(:PubliclyAccessible) { true }
-          r.property(:DBSnapshotIdentifier) { ENV["SNAPSHOTID"]}
-                  end
-
-                end
-
-              end
-
-            end
+          r.property(:db_instance_class) {"db.t2.micro"}
+          r.property(:db_subnet_groupname) {:db_subnet_group.ref}
+          r.property(:engine) {:engine_type.ref}
+          r.property(:master_username) { ENV["DB_Username"] }
+          r.property(:master_userpassword) { ENV["DB_Password"] }
+          r.property(:allocated_storage) {:db_allocated_storage.ref}
+          r.property(:publicly_accessible) { true }
+          r.property(:db_snapshot_identifier) { ENV["SNAPSHOTID"]}
+          end
+        end
+    end
+end
