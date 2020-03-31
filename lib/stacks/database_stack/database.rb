da8194dfc14600c963dbeb2ffbe148f4a8ef19f4
Joel Grimberg <jgrimberg@xebia.com>
2020-03-31 12:43:31 +0200

    fix rubocop warnings

--- lib/stacks/database_stack/database.rb
+++ lib/stacks/database_stack/database.rb
@@ -1,32 +1,32 @@
 module DatabaseStack
-    module Database
-      extend ActiveSupport::Concern
-      included do
-        parameter :engine_type,
-                  default: "MySQL"
-        parameter :db_allocated_storage,
-                  default: "50"
-        parameter :subnets,
-                  description: "Subnets for the Database"
+  module Database
+    extend ActiveSupport::Concern
+    included do
+      parameter :engine_type,
+                default: "MySQL"
+      parameter :db_allocated_storage,
+                default: "50"
+      parameter :subnets,
+                description: "Subnets for the Database"
 
-        resource :db_subnet_group,
-                  type: "AWS::RDS::DBSubnetGroup" do |r|
-          r.property(:db_subnet_groupname) {"db_subnet_group"}
-          r.property(:db_subnet_groupdescription) {"subnetgroup for DB"}
-          r.property(:subnet_ids) {:subnets.ref.fnsplit(",")}
-        end
+      resource :db_subnet_group,
+               type: "AWS::RDS::DBSubnetGroup" do |r|
+        r.property(:db_subnet_groupname) { "db_subnet_group" }
+        r.property(:db_subnet_groupdescription) { "subnetgroup for DB" }
+        r.property(:subnet_ids) { :subnets.ref.fnsplit(",") }
+      end
 
-        resource :my_database,
-                  type: "AWS::RDS::DBInstance" do |r|
-          r.property(:db_instance_class) {"db.t2.micro"}
-          r.property(:db_subnet_groupname) {:db_subnet_group.ref}
-          r.property(:engine) {:engine_type.ref}
-          r.property(:master_username) { ENV["DB_Username"] }
-          r.property(:master_userpassword) { ENV["DB_Password"] }
-          r.property(:allocated_storage) {:db_allocated_storage.ref}
-          r.property(:publicly_accessible) { true }
-          r.property(:db_snapshot_identifier) { ENV["SNAPSHOTID"]}
-          end
-        end
+      resource :my_database,
+               type: "AWS::RDS::DBInstance" do |r|
+        r.property(:db_instance_class) { "db.t2.micro" }
+        r.property(:db_subnet_groupname) { :db_subnet_group.ref }
+        r.property(:engine) { :engine_type.ref }
+        r.property(:master_username) { ENV["DB_Username"] }
+        r.property(:master_userpassword) { ENV["DB_Password"] }
+        r.property(:allocated_storage) { :db_allocated_storage.ref }
+        r.property(:publicly_accessible) { true }
+        r.property(:db_snapshot_identifier) { ENV["SNAPSHOTID"] }
+      end
     end
+  end
 end
--- lib/stacks/parent_stack/parent.rb
+++ lib/stacks/parent_stack/parent.rb
@@ -7,36 +7,34 @@ module InfraStack
       generate_bootstrap_parameters
 
       resource :vpc_stack,
-                type: "AWS::CloudFormation::Stack" do |r|
+               type: "AWS::CloudFormation::Stack" do |r|
         r.property(:template_url) { "vpcstack" }
         r.property(:tags) { default_tags }
       end
 
       resource :database_stack,
-                type: "AWS::CloudFormation::Stack" do |r|
+               type: "AWS::CloudFormation::Stack" do |r|
         r.property(:template_url) { "databasestack" }
-        r.property(:parameters) do |r|
-          {
-            "Subnets":
-            [
-              :vpc_stack.ref("Outputs.BackendRdsPublicSubnetName"),
-              :vpc_stack.ref("Outputs.BackendRdsPublicSubnet2Name"),
-              :vpc_stack.ref("Outputs.BackendRdsPublicSubnet3Name")
-            ].fnjoin(",")
-          }
-        end
+        r.property(:parameters)
+        {
+          "Subnets": [
+            :vpc_stack.ref("Outputs.BackendRdsPublicSubnetName"),
+            :vpc_stack.ref("Outputs.BackendRdsPublicSubnet2Name"),
+            :vpc_stack.ref("Outputs.BackendRdsPublicSubnet3Name"),
+          ].fnjoin(","),
+        }
       end
 
       resource :acm_stack,
-                amount: 0,
-                type: "AWS::CloudFormation::Stack" do |r|
+               amount: 0,
+               type: "AWS::CloudFormation::Stack" do |r|
         r.property(:template_url) { "acmstack" }
         r.property(:tags) { default_tags }
       end
 
       resource :ecs_stack,
-                amount: 0,
-                type: "AWS::CloudFormation::Stack" do |r|
+               amount: 0,
+               type: "AWS::CloudFormation::Stack" do |r|
         r.depends_on %w(VpcStack)
         r.property(:template_url) { "ecsstack" }
         r.property(:parameters) do
@@ -45,13 +43,13 @@ module InfraStack
             "Subnets": [
               :vpc_stack.ref("Outputs.Ec2PrivateSubnetName"),
               :vpc_stack.ref("Outputs.Ec2PrivateSubnet2Name"),
-              :vpc_stack.ref("Outputs.Ec2PrivateSubnet3Name")
+              :vpc_stack.ref("Outputs.Ec2PrivateSubnet3Name"),
             ].fnjoin(","),
             "PublicSubnets": [
               :vpc_stack.ref("Outputs.Ec2PublicSubnetName"),
               :vpc_stack.ref("Outputs.Ec2PublicSubnet2Name"),
-              :vpc_stack.ref("Outputs.Ec2PublicSubnet3Name")
-            ].fnjoin(",")
+              :vpc_stack.ref("Outputs.Ec2PublicSubnet3Name"),
+            ].fnjoin(","),
           }
         end
       end
